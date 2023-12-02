2255d8da47d8750165b65331be879f0b974cd179
Logan Cadman <61567751+Lcadman@users.noreply.github.com>
2023-12-02 16:27:12 -0700

    added some stuff

---
+++ interface.py
@@ -0,0 +1,57 @@
+import pygame
+import sys
+from game import Board  # Import your existing game class
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the display
+width, height = 400, 400
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("2048 Game")
+
+# Colors
+background_color = (250, 248, 239)
+tile_colors = {0: (205, 193, 180), 2: (238, 228, 218), 4: (237, 224, 200), 
+               8: (242, 177, 121), 16: (245, 149, 99), 32: (246, 124, 95), 
+               64: (246, 94, 59), 128: (237, 207, 114), 256: (237, 204, 97), 
+               512: (237, 200, 80), 1024: (237, 197, 63), 2048: (237, 194, 46)}
+
+# Font
+font = pygame.font.SysFont("arial", 40)
+
+# Create a game instance
+game = Board()
+
+def draw_board():
+    screen.fill(background_color)
+    for i, row in enumerate(game.board):
+        for j, value in enumerate(row):
+            tile_color = tile_colors[value]
+            pygame.draw.rect(screen, tile_color, (j*100, i*100, 100, 100))
+            if value != 0:
+                text_surface = font.render(str(value), True, (0, 0, 0))
+                text_rect = text_surface.get_rect(center=(j*100 + 50, i*100 + 50))
+                screen.blit(text_surface, text_rect)
+
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                game.move_tiles('u')
+            # Handle other keys similarly...
+            # ...
+
+    # Draw the game board
+    draw_board()
+
+    # Update the display
+    pygame.display.flip()
+
+# Quit Pygame
+pygame.quit()
+
