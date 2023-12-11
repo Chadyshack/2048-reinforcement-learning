ddd62d49bee5007f8253c97343682de5b4c64740
Chad Schwenke <chad.schwenke@colostate.edu>
2023-12-11 12:48:39 -0700

    Cleaned up and commented all the code

--- game.py
+++ game.py
@@ -2,13 +2,16 @@ import random
 
 class Board:
     def __init__(self):
+        # Declare variables for score, last added tile position, merges made in last move, and game over flag
         self.score = 0
-        self.game_over = False
-        self.board = [[0]*4 for _ in range(4)]
-        self._add_new_tile()
-        self._add_new_tile()
         self.last_added_tile = None
         self.merges_in_last_move = 0
+        self.game_over = False
+
+        # Initialize board with all zeros (4x4 grid) and then add two tiles
+        self.board = [[0] * 4 for _ in range(4)]
+        self._add_new_tile()
+        self._add_new_tile()
 
     def print_board(self):
         # Iterates through each row of the board and prints it
@@ -20,33 +23,46 @@ class Board:
         print(f"Score: {self.score}")
 
     def move_tiles(self, direction):
+        # Check if game is already over
         if self.game_over:
             return False
 
+        # Reset merge count to zero
         self.merges_in_last_move = 0
 
+        # Make a copy of the original board
         original_board = [row[:] for row in self.board]
+
+        # Transpose board for up and down moves
         if direction in ('u', 'd'):
             self._transpose_board()
 
+        # Run logic for all rows or columns in move
         for i in range(4):
+            # Up and left are treated the same after transpose
             if direction in ('u', 'l'):
                 shifted_row = self._shift(self.board[i])
                 self.board[i] = self._merge(shifted_row)
-            else:  # 'd' or 'r'
+            # Down and right must be reversed first since functions treat everything as left
+            elif direction in ('d', 'r'):
                 reversed_row = list(reversed(self.board[i]))
                 shifted_row = self._shift(reversed_row)
                 merged_row = self._merge(shifted_row)
+                # Undo reverse
                 self.board[i] = list(reversed(merged_row))
 
+        # Run transpose again to reset
         if direction in ('u', 'd'):
             self._transpose_board()
 
+        # Check if board changed from original
         if self.board != original_board:
+            # Add a new tile and then check if game is over
             self._add_new_tile()
             if not self._moves_available():
                 self.game_over = True
             return True
+        # If board did not change from original, the move was invalid but game is not yet over
         return False
 
     def _transpose_board(self):
@@ -77,18 +93,22 @@ class Board:
                 row[i] *= 2
                 row[i + 1] = 0
                 self.score += row[i]
+                # Update the number of merges
                 self.merges_in_last_move += 1
-        return self._shift(row)  # Shift again to ensure tiles are properly aligned
+        # Shift again to ensure tiles are properly aligned
+        return self._shift(row)
 
     def _add_new_tile(self):
-        # Adds a new tile (2 or 4) to a random empty position on the board
+        # Check for possible empty positions
         empty_positions = []
         for i in range(4):
             for j in range(4):
+                # If cell is zero it is empty
                 if self.board[i][j] == 0:
                     empty_positions.append((i, j))
         if empty_positions:
             i, j = random.choice(empty_positions)
+            # Add a 2 or 4 to a random empty position (10% chance to be 4, 90% chance to be 2)
             self.board[i][j] = 4 if random.random() < 0.1 else 2
             self.last_added_tile = (i, j)
 
@@ -109,17 +129,26 @@ class Board:
 
 # The main function for running the game
 def main():
-    game = BoardClass()
+    # Make game object and set of allowed moves
+    game = Board()
     move_commands = {'u': 'up', 'd': 'down', 'l': 'left', 'r': 'right'}
 
+    # Loop until game over
     while not game.game_over:
+        # Print the board and score, prompt user for move
         game.print_board()
         game.print_score()
         move = input("Enter your move (u, d, l, r): ").lower()
+        # Check if move was not possible and notify user if so
         if move in move_commands:
             if not game.move_tiles(move):
                 print("Move not possible. Try a different direction.")
         else:
             print("Invalid input. Please enter 'u', 'd', 'l', or 'r'.")
+        # Check if game is over and notify user if so
         if game.game_over:
             print("Game over! Your final score is:", game.score)
+
+# Run main function
+if __name__ == "__main__":
+    main()
--- interface.py
+++ interface.py
@@ -1,57 +1,49 @@
 import pygame
-import sys
-from game import Board  # Import your existing game class
+from game import Board
 
 # Initialize Pygame
 pygame.init()
 
-# Set up the display
-width, height = 400, 450  # Increase height to 450 to add space for the score
+# Set up the display and caption
+width, height = 400, 450
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("2048 Game")
 
-# Colors
+# Set styling for colors and font
 background_color = (250, 248, 239)
 tile_colors = {0: (205, 193, 180), 2: (238, 228, 218), 4: (237, 224, 200), 
                8: (242, 177, 121), 16: (245, 149, 99), 32: (246, 124, 95), 
                64: (246, 94, 59), 128: (237, 207, 114), 256: (237, 204, 97), 
                512: (237, 200, 80), 1024: (237, 197, 63), 2048: (237, 194, 46)}
-
-# Font
 font = pygame.font.SysFont("arial", 40)
 
 # Create a game instance
 game = Board()
 
+# Function to draw game board
 def draw_board():
+    # Fill screen with background color
     screen.fill(background_color)
+    # Loop over each value in the game board
     for i, row in enumerate(game.board):
         for j, value in enumerate(row):
-            # Default color for tiles
-            tile_color = tile_colors.get(value, (255, 255, 255))  # White for unexpected values
-            pygame.draw.rect(screen, tile_color, (j*100, i*100, 100, 100))
+            # Get the color for this value (white if missing) and draw rectangle on screen
+            tile_color = tile_colors.get(value, (255, 255, 255))
+            pygame.draw.rect(screen, tile_color, (j * 100, i * 100, 100, 100))
+            # If the value was not zero, draw the numerical value on this tile's center as well
             if value != 0:
-                text_surface = font.render(str(value), True, (0, 0, 0))
-                text_rect = text_surface.get_rect(center=(j*100 + 50, i*100 + 50))
+                # If we are at the index of the last added tile, draw it red for distinction
+                if (i, j) == game.last_added_tile:
+                    text_surface = font.render(str(value), True, (255, 0, 0))
+                else:
+                    text_surface = font.render(str(value), True, (0, 0, 0))
+                text_rect = text_surface.get_rect(center=(j * 100 + 50, i * 100 + 50))
                 screen.blit(text_surface, text_rect)
-
-    # Highlight the last added tile with a distinct color
-    if game.last_added_tile:
-        i, j = game.last_added_tile
-        last_tile_color = (255, 255, 255)  # Example: white color for the new tile
-        pygame.draw.rect(screen, last_tile_color, (j*100, i*100, 100, 100))
-        value = game.board[i][j]
-        if value != 0:
-            text_surface = font.render(str(value), True, (0, 0, 0))
-            text_rect = text_surface.get_rect(center=(j*100 + 50, i*100 + 50))
-            screen.blit(text_surface, text_rect)
 
     # Draw the score below the game board
     score_text = font.render(f"Score: {game.score}", True, (0, 0, 0))
-    score_rect = score_text.get_rect(center=(width // 2, height - 25))  # Position the score at the bottom
+    score_rect = score_text.get_rect(center=(width // 2, height - 25))
     screen.blit(score_text, score_rect)
-
-
 
 # Main game loop
 running = True
@@ -59,6 +51,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # Get input from arrow keys or WASD on users keyboard for moves
         if event.type == pygame.KEYDOWN:
             if event.key in (pygame.K_w, pygame.K_UP):
                 game.move_tiles('u')
@@ -77,4 +70,3 @@ while running:
 
 # Quit Pygame
 pygame.quit()
-
